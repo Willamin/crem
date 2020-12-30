314e563c1db91371b36cb3a825bcd5a4be2b75f9
Will Lewis <will@wflewis.com>
2020-12-30 14:20:03 -0500

    Improve cli handling

--- src/server.cr
+++ src/server.cr
@@ -2,30 +2,39 @@ require "colorize"
 require "option_parser"
 require "gemini"
 
-server_address = "0.0.0.0"
-server_port = 1965
-server_cert_chain = nil
-server_private_key = nil
+class Cascading(Value)
+  property default : Value
+  property config_option : Value?
+  property cli_flag : Value?
+  property env_var : Value?
 
-if env_value = ENV["CREM_ADDRESS"]?
-  server_address = env_value
+  def initialize(@default); end
+
+  def unwrap : Value
+    @env_var || @cli_flag || @config_option || @default
+  end
 end
-if env_value = ENV["CREM_PORT"]?.try(&.to_i32)
-  server_port = env_value
+
+struct Config
+  property address = Cascading(String).new("0.0.0.0")
+  property port = Cascading(Int32).new(1965)
+  property cert_chain = Cascading(String?).new(nil)
+  property private_key = Cascading(String?).new(nil)
 end
-if env_value = ENV["CREM_CERT"]?
-  server_cert_chain = env_value
-end
-if env_value = ENV["CREM_KEY"]?
-  server_private_key = env_value
-end
+
+config = Config.new
+
+config.address.env_var = ENV["CREM_ADDRESS"]?
+config.port.env_var = ENV["CREM_PORT"]?.try(&.to_i32?)
+config.cert_chain.env_var = ENV["CREM_CERT"]?
+config.private_key.env_var = ENV["CREM_KEY"]?
 
 parser = OptionParser.new do |parser|
 parser.banner = "Usage: crem-server [options]"
-  parser.on("--address=ADDR", "Specify the address to bind") { |addr| server_address = addr }
-  parser.on("--port=PORT", "Specify the port to bind") { |port| server_port = port.to_i32 }
-  parser.on("--cert=FILE", "Specify the certificate chain file") { |file| server_cert_chain = file }
-  parser.on("--key=FILE", "Specify the private key file") { |file| server_private_key = file }
+  parser.on("--address=ADDR", "Specify the address to bind") { |addr| config.address.cli_flag = addr }
+  parser.on("--port=PORT", "Specify the port to bind") { |port| config.port.cli_flag = port.to_i32 }
+  parser.on("--cert=FILE", "Specify the certificate chain file") { |file| config.cert_chain.cli_flag = file }
+  parser.on("--key=FILE", "Specify the private key file") { |file| config.private_key.cli_flag = file }
 
   parser.on("--help", "show this help") do
     puts(parser)
@@ -45,12 +54,12 @@ server = Gemini::Server.new([
   Gemini::Server::StaticHandler.new("."),
 ])
 
-server.certificate_chain = server_cert_chain
-server.private_key = server_private_key
+server.certificate_chain = config.cert_chain.unwrap
+server.private_key = config.private_key.unwrap
 
 begin
-  spawn { puts("listening on gemini://#{server.address}:#{server.port}") }
-  server.listen
+  spawn { puts("listening on gemini://#{config.address.unwrap}:#{config.port.unwrap}") }
+  server.listen(config.address.unwrap, config.port.unwrap)
 rescue e : Gemini::Server::MissingCertificateChain
   puts("You must provide a certificate chain to serve Gemini content.".colorize(:red))
   puts("Use the --cert flag or the CREM_CERT environment variable to specify.".colorize(:yellow))
